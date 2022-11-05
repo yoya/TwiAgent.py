5680082107f7f8abddef81faf17082fadf358e69
Yoshihiro Yamazaki <yoya@awm.jp>
2022-11-06 01:29:26 +0900

    ブックマーク画面を開くところまで。 JSON のエラー処理。

--- TwiBookmaDL.py
+++ TwiBookmaDL.py
@@ -1,12 +1,21 @@
 # pip install selenium
 
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 class TwiBookmaDL:
-    options = Options()
-    driver = webdriver.Chrome(options=options)
     def __init__(self):
+        options = Options()
+        self.driver = webdriver.Chrome(options=options)
     def openBrowser(self, url, cookieFile):
-        print('openBrowser', url, cookieFile)
-
+        self.driver.get(url)
+        json_open = open(cookieFile, 'r')
+        try:
+            cookies = json.load(json_open)
+            for cookie in cookies:
+                tmp = {"name": cookie["name"], "value": cookie["value"]}
+                self.driver.add_cookie(tmp)
+        except json.decoder.JSONDecodeError as e:
+            mesg = "Invalid Cookie JSON file({}) => {}".format(cookieFile, e)
+            raise Exception(mesg)
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # (c) 2022/05/22 yoya@awn.jp
 
-import sys
+import sys, time
 from TwiBookmaDL import TwiBookmaDL
+import json
 
-prog, url, cookieFile = sys.argv;
+prog, cookieFile = sys.argv;
+
+BOOKMARK_URL = "https://twitter.com/i/bookmarks"
 
 dl = TwiBookmaDL()
-dl.openBrowser(url, cookieFile)
+try:
+    dl.openBrowser(BOOKMARK_URL, cookieFile)
+except Exception as e:
+    print(e)
+    exit (1)
+    
+time.sleep(5)
