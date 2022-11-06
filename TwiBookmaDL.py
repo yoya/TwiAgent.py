16406c7db14c6c1d5762a2ed2f6399c70871f226
Yoshihiro Yamazaki <yoya@awm.jp>
2022-11-07 02:52:54 +0900

    readBookmarkArticle => text, imgsrcs

--- TwiBookmaDL.py
+++ TwiBookmaDL.py
@@ -1,6 +1,6 @@
 # pip install selenium
 
-import time
+#import time
 import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -12,24 +12,54 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class TwiBookmaDL:
     def __init__(self):
-        options = Options()
-        self.driver = webdriver.Chrome(options=options)
+        pass
+    # ブラウザを開く
     def openBrowser(self, url, cookieFile):
-        self.driver.get(url)
         json_open = open(cookieFile, 'r')
         try:
             cookies = json.load(json_open)
+            options = Options()
+            self.driver = webdriver.Chrome(options=options)
+            self.driver.get(url)
             for cookie in cookies:
                 tmp = {"name": cookie["name"], "value": cookie["value"]}
                 self.driver.add_cookie(tmp)
         except json.decoder.JSONDecodeError as e:
             mesg = "Invalid Cookie JSON file({}) => {}".format(cookieFile, e)
             raise Exception(mesg)
+    # ブックマーク一覧を取得する
     def readBookmarkArticles(self):
         locator = (By.CSS_SELECTOR, 'article')
-        WebDriverWait(self.driver, 10).until(
+        wait = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located(locator)
         )
+        print("wait", wait)
         articles = self.driver.find_elements(*locator)
         print("articles count:{}".format(len(articles)))
         return articles
+    # ブックマーク情報の１つから文字と画像を収集する
+    def readBookmarkArticle(self, article):
+        texts = article.find_elements(By.CSS_SELECTOR, 'div[data-testid="tweetText"]')
+        text = texts[0]
+        photos = article.find_elements(By.CSS_SELECTOR, 'div[data-testid="tweetPhoto"]')
+        photo = photos[0]
+        imgs = photo.find_elements(By.CSS_SELECTOR, 'img')
+        imgsrcs = []
+        for img in imgs:
+            print("img", img, img.get_attribute('innerHTML'))
+            src = img.get_attribute("src")
+            imgsrcs.append(src)
+        return text.text, imgsrcs
+    # 共有ボタンをクリックして、ポップアップめんニューを出す
+    def clickBookmarkShareButton(self, article):
+        print("clickBookmarkShareButton", article)
+        svg = article.find_elements(By.CSS_SELECTOR, 'svg')
+        print(svg)
+#        locator = (By.CSS_SELECTOR, 'div[aria-label="Share Tweet"]')
+#        menus = article.find_elements(*locator)
+        menus = article.find_elements(By.CSS_SELECTOR, 'div[aria-label="Share Tweet"]')
+        menusLen = len(menus)
+        if menusLen != 1:
+            print("menus count:{}".format(menusLen))
+            raise Exception("article has no menus")
+        self.driver.execute_script('arguments[0].click();', menus[0])
--- main.py
+++ main.py
@@ -11,8 +11,21 @@ BOOKMARK_URL = "https://twitter.com/i/bookmarks"
 dl = TwiBookmaDL()
 try:
     dl.openBrowser(BOOKMARK_URL, cookieFile)
-    articles = dl.readBookmarkArticles()
+    while (True):
+        articles = dl.readBookmarkArticles()
+        articlesLen = len(articles)
+        print("articles count:{}".format(articlesLen))
+        if (articlesLen < 1):
+            time.sleep(10)
+            continue
+        for article in articles:
+            text, imgsrcs = dl.readBookmarkArticle(article)
+            print(text, imgsrcs)
+#            dl.clickBookmarkShareButton(article)
+            time.sleep(10)
+        break
+        
 except Exception as e:
-    print(e)
+    print(e, file=sys.stderr)
     exit (1)
 
