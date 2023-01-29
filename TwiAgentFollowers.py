9c1be24cb839e7f8197183cb29f70c385e1b168a
Yoshihiro Yamazaki <yoya@awm.jp>
2023-01-29 20:10:25 +0900

    フォロワー一覧機能。（作りかけ)

--- TwiAgentFollowers.py
+++ TwiAgentFollowers.py
@@ -1,4 +1,6 @@
 from TwiAgent import TwiAgent
+import time
+from selenium.common.exceptions import StaleElementReferenceException
 
 class TwiAgentFollowers(TwiAgent):
     FOLLOWERS_URL = "https://twitter.com/followers"
@@ -6,9 +8,29 @@ class TwiAgentFollowers(TwiAgent):
         url = self.FOLLOWERS_URL
         self.openBrowser(url, cookieFile)
     def readFollowers(self):
-#        followers = self.readByCSSSelector(self.driver, 'div[aria-label="Timeline: Followers"]', wait=True)
-        followers = self.readByCSSSelectorAll(self.driver, 'div[data-testid="cellInnerDiv"]', wait=True)
-        print(followers)
-
-        
-        
+#        followers = self.readByCSSSelectorAll(self.driver, 'div[data-testid="cellInnerDiv"]', wait=True)
+        followersUsername = []
+        done = False
+        while not done:
+            followers = self.readByCSSSelectorAll(self.driver, 'div[data-testid="UserCell"]', wait=True)
+            print(len(followers))
+            try:
+                done = True
+                for f in followers:
+                    xpath = './/a/div/div/span[contains(text(),"@")]';
+                    users = self.readByXPATHAll(f, xpath, wait=True)
+                    for u in users:
+                        print(u.text)
+                        if u.text not in followersUsername:
+                            followersUsername.append(u.text)
+                            done = False
+                if not done:
+                    print("scroll")
+                    self.scrollToBottom()
+            except StaleElementReferenceException as e:
+                print("stale element")
+                done = False
+                time.sleep(1)
+                break
+            time.sleep(3)
+        return followersUsername
