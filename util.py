38bd5b3269604a43bc4bb029357476af575b9b00
Yoshihiro Yamazaki <yoya@awm.jp>
2023-03-17 23:05:28 +0900

    youtubeDl function

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
-import os, base64
+import os, base64, subprocess
+
+YouTubeDL = "youtube-dl"
 
 # https://iterm2.com/documentation-one-page.html#documentation-images.html
 # ESC]1337;File=name={};size={};inline=1;height={}:   .. ^G\n
@@ -18,6 +20,17 @@ def imgcat(filename, height):
 # import os, sys
 #if os.environ["TERM_PROGRAM"] == "iTerm.app":
 #    imgcat(sys.argv[1], 7)
+
+def youtubeDl(url, filename):
+    try:
+        result = subprocess.run([YouTubeDL, "-help"], capture_output=True)
+    except FileNotFoundError as e:
+        return
+    result = subprocess.run([YouTubeDL, url, "-o", filename])
+
+#url = "https://twitter.com/yoya/status/1636393346452291588"
+#folder = "1636393346452291588.mp4"
+#youtubeDl(url, folder)
 
 def htmldump(element):
     print(element.get_attribute('outerHTML'))
